c4be64b74a0b22345e6c204e521e6beb5084b202
apalski <annette.drapalski@gmail.com>
2017-05-01 10:22:33 +1000

    edited city_controller_sped deleted country_controller_spec and user_controller_spec limiting rspec testing

--- spec/controllers/city_controller_spec.rb
+++ spec/controllers/city_controller_spec.rb
@@ -25,30 +25,10 @@ describe CityController do
 	end
 
 	describe "/cities" do
-		visit '/cities'
 		it "can visit '/cities'" do
 			get '/cities'
 			expect(last_response.status).to eq(200)
 		end	
-			
-		it "displays a list of cities and their country" do
-			visit "/cities"
-			expect(page).to have_content("Sydney")
-			expect(page).to have_content("Australia")
-		end
 	end	
-
-	describe "/cities/new"
-		it "can create a new city and associate it to its country" do
-			visit '/cities/new'
-			fill_in :city_name, :with => "Lima"
-			fill_in :country_name, :with => "Peru"
-			click_button "Create City"
-			expect(City.all.count).to eq(3)
-			expect(City.all[2].name).to eq("Lima")
-
-		end	
-
-		it 'can associate the new city with its user through its country' do
-		end	
 end	
+
